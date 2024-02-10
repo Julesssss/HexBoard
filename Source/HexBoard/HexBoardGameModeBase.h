a1de5e983b0a63f5c5d38f66d21a4f9d680acc45
jules <julianrosser91@gmail.com>
2024-02-10 10:54:29 +0000

    create new project

---
+++ Source/HexBoard/HexBoardGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HexBoardGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXBOARD_API AHexBoardGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
